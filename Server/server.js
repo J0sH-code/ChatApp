a42154bff4d19af8d5eaf286b63cb533760a0432
J0sH-code <joshrylesanteno07@gmail.com>
2026-01-20 23:54:40 +0800

    Minor syntax change

--- Server/server.js
+++ Server/server.js
@@ -1,18 +1,19 @@
 import app from "./http/express.js";
-import { Server } from 'socket.io';
-import socketServer from "./Socket/SocketServer.js";
+import { Server } from "socket.io";
+import socketServer from "./Socket/socketServer.js";
 
 const port = process.env.port || 3000;
-const serverExpress = app.listen(port, () => console.log(`Listening at port ${port}`));
-
+const serverExpress = app.listen(port, () =>
+  console.log(`Listening at port ${port}`),
+);
 
 export const io = new Server(serverExpress, {
-    cors: {
-        origin: "*",
-        methods: ["GET","POST"]
-    }   
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on("connection", (socket) => {
-    socketServer(socket);
-})
+  socketServer(socket);
+});
