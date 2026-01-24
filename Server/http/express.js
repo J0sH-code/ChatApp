9053969cd5aaabfbebd8c59083a8c51ede0d6f4c
J0sH-code <joshrylesanteno07@gmail.com>
2026-01-24 23:46:00 +0800

    Wrote basic express routes for future RESTful operations

--- Server/http/express.js
+++ Server/http/express.js
@@ -4,4 +4,20 @@ const app = express();
 app.use(express.static("../Client"));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send({ status: "Get route working" });
+})
+
+app.post("/post", (req, res) => {
+    res.send({ status: "Get route working" });
+})
+
+app.patch("/patch", (req, res) => {
+    res.send({ status: "Get route working" });
+})
+
+app.delete("/delete", (req, res) => {
+    res.send({ status: "Get route working" });
+})
+
 export default app;
