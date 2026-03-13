3eb169271f6520aef9d60848609ab7ed07089e39
J0sH-code <joshrylesanteno07@gmail.com>
2026-03-13 12:38:39 +0800

    new render url

--- Client/socketClient.js
+++ Client/socketClient.js
@@ -12,7 +12,7 @@ import {
 } from "./ui.js";
 import { popUpAcceptBtn, popUpIgnoreBtn } from "./domElements.js";
 
-export const socket = io("https://chatapp-hrjj.onrender.com");
+export const socket = io("https://chatapp-1-91a9.onrender.com");
 
 /**
  * Setup all socket event listeners
